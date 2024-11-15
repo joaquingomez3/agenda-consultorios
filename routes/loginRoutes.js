6a24cc1b67f523eb8ecfe856613dd100961e3306
joaquingomez3 <gomezgonzalod46@gmail.com>
2024-11-14 22:29:28 -0300

    rutas

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/loginController');
 
-router.get('/login', authController.mostrarLogin);
+router.get('/', authController.mostrarLogin);
 router.post('/login', authController.procesarLogin);
 
 module.exports = router;
